49f2c85825281dacec4399747e36257e24ffa2dd
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2024-08-18 16:15:25 -0500

    prod

--- backend/src/routes/scrapeRoutes.ts
+++ backend/src/routes/scrapeRoutes.ts
@@ -1,10 +1,9 @@
 import express from 'express';
-import { scrapeWebsite, saveScrapedData, getUserScrapedData } from '../controllers/scrapeController'; 
+import { scrapeWebsite, getUserScrapedData } from '../controllers/scrapeController'; 
 
 const router = express.Router();
 
 router.post('/scrape', scrapeWebsite);  // Endpoint for scraping
-router.post('/save', saveScrapedData);  // Endpoint for saving scraped data
 router.get('/my-data', getUserScrapedData);  // Endpoint for getting user's scraped data
 
 export default router;
